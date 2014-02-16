ce55a8ac180aa7033d06d0092a2da02ff6bc0b06
Jennifer <jennifergapay@gmail.com>
2014-02-16 15:01:06 -0500

    hide my keys

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -21,6 +21,6 @@ class DealsController < ApplicationController
   end
 
   def brunch_deals(zip, milesradius)
-    return HTTParty.get("http://api.8coupons.com/v1/getdeals?key=d279da9cc6f9b696af1931f3653f21cebddb6ccbbcbc783d7fad2055742e015fe9915cc8f5ba5b50ead16413ba4d62a9&zip=#{zip}&mileradius=#{milesradius}&limit=500&userid=18381&search=brunch")
+    return HTTParty.get("http://api.8coupons.com/v1/getdeals?key=8COUPONS_CLIENT_ID&zip=#{zip}&mileradius=#{milesradius}&limit=500&userid=18381&search=brunch")
   end
 end
---
+++ db/migrate/20140216194504_add_zip_to_users.rb
@@ -0,0 +1,4 @@
+class AddZipToUsers < ActiveRecord::Migration
+  def change
+  end
+end
